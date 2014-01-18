e03aeef9ff88bb9883a7bec4817b19d074c94214
nuxsmin <nuxsmin@cygnux-pc>
2014-01-18 21:41:11 +0100

    Fixed upgrade error caused by invalid query.

--- inc/upgrade.class.php
+++ inc/upgrade.class.php
@@ -51,15 +51,15 @@ class SP_Upgrade {
     }
 
     private static function upgradeTo($version) {
-        self::$result['action'][] = _('Actualizar BBDD');
+        self::$result['action'] = _('Actualizar BBDD');
         
         switch ($version) {
             case 110:
                 $queries[] = "ALTER TABLE `accFiles` CHANGE COLUMN `accfile_name` `accfile_name` VARCHAR(100) NOT NULL";
                 $queries[] = "ALTER TABLE `accounts` ADD COLUMN `account_otherGroupEdit` BIT(1) NULL DEFAULT 0 AFTER `account_dateEdit`, ADD COLUMN `account_otherUserEdit` BIT(1) NULL DEFAULT 0 AFTER `account_otherGroupEdit`;";
                 $queries[] = "CREATE TABLE `accUsers` (`accuser_id` INT NOT NULL AUTO_INCREMENT,`accuser_accountId` INT(10) UNSIGNED NOT NULL,`accuser_userId` INT(10) UNSIGNED NOT NULL, PRIMARY KEY (`accuser_id`), INDEX `idx_account` (`accuser_accountId` ASC));";
-                $queries[] = "ALTER TABLE `syspass`.`accHistory` ADD COLUMN `accHistory_otherUserEdit` BIT NULL AFTER `acchistory_mPassHash`, ADD COLUMN `accHistory_otherGroupEdit` VARCHAR(45) NULL AFTER `accHistory_otherUserEdit`;";
-                $queries[] = "ALTER TABLE `syspass`.`accFiles` CHANGE COLUMN `accfile_type` `accfile_type` VARCHAR(100) NOT NULL ;";
+                $queries[] = "ALTER TABLE `accHistory` ADD COLUMN `accHistory_otherUserEdit` BIT NULL AFTER `acchistory_mPassHash`, ADD COLUMN `accHistory_otherGroupEdit` VARCHAR(45) NULL AFTER `accHistory_otherUserEdit`;";
+                $queries[] = "ALTER TABLE `accFiles` CHANGE COLUMN `accfile_type` `accfile_type` VARCHAR(100) NOT NULL ;";
                 break;
             default :
                 self::$result['text'][] = _('No es necesario actualizar la Base de Datos.');
@@ -67,7 +67,9 @@ class SP_Upgrade {
         }
         
         foreach ($queries as $query) {
-            if (DB::doQuery($query, __FUNCTION__) === FALSE) {
+            if (DB::doQuery($query, __FUNCTION__) === FALSE 
+                    && DB::$numError != 1060 
+                    && DB::$numError != 1050) {
                 self::$result['text'][] = _('Error al aplicar la actualización de la Base de Datos.');
                 return FALSE;
             }
