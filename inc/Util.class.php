7189538a081839155268efaf6c799ef96953f900
nuxsmin <nuxsmin@syspass.org>
2016-09-21 10:51:08 +0200

    * [FIX] Fixed LDAP mail retrieving when multiple mail addresses are set. Thanks to @ArminXG. Closes #278 * [FIX] Fixed user migration process. * [MOD] Add LDAP mail mapping. Related #278 * [MOD] Update version

--- inc/Util.class.php
+++ inc/Util.class.php
@@ -309,7 +309,7 @@ class Util
      */
     public static function getVersion($retBuild = false)
     {
-        $version = array(1, 2, 0, '14');
+        $version = array(1, 2, 0, '15');
 
         if (!$retBuild) {
             array_pop($version);
