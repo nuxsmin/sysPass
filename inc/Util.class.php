c95a27b32d7327d89520d70b02e066fc39d12cea
nuxsmin <nuxsmin@syspass.org>
2016-10-02 23:59:00 +0200

    * [MOD] Update version

--- inc/Util.class.php
+++ inc/Util.class.php
@@ -309,7 +309,7 @@ class Util
      */
     public static function getVersion($retBuild = false)
     {
-        $version = array(1, 2, 0, '17');
+        $version = array(1, 2, 0, '18');
 
         if (!$retBuild) {
             array_pop($version);
