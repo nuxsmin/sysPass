a3c44ad310a668db83538ad9eb4a1eaebd43f478
nuxsmin <nuxsmin@syspass.org>
2017-01-24 15:49:04 +0100

    * [MOD] Updated build number

--- inc/SP/Util/Util.class.php
+++ inc/SP/Util/Util.class.php
@@ -367,7 +367,7 @@ class Util
      */
     public static function getVersion($retBuild = false)
     {
-        $build = '17012401';
+        $build = '17012402';
         $version = [2, 0, 0];
 
         if ($retBuild) {
