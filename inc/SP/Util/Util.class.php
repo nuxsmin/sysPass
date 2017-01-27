5aca776bc8cc9f483a7bb79900152a1decf10d55
nuxsmin <nuxsmin@syspass.org>
2017-01-27 13:35:10 +0100

    * [MOD] Bump to RC1

--- inc/SP/Util/Util.class.php
+++ inc/SP/Util/Util.class.php
@@ -174,7 +174,7 @@ class Util
      */
     public static function getVersionString()
     {
-        return '2.0-beta';
+        return '2.0-RC1';
     }
 
     /**
@@ -368,7 +368,7 @@ class Util
      */
     public static function getVersion($retBuild = false)
     {
-        $build = '17012601';
+        $build = '17012701';
         $version = [2, 0, 0];
 
         if ($retBuild) {
