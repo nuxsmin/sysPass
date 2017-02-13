59fbdba807000a512c43c9a8b85a3351c5c8f8e9
nuxsmin <nuxsmin@syspass.org>
2017-02-13 09:06:56 +0100

    * [MOD] Bump buil number

--- inc/SP/Util/Util.class.php
+++ inc/SP/Util/Util.class.php
@@ -368,7 +368,7 @@ class Util
      */
     public static function getVersion($retBuild = false)
     {
-        $build = '17021301';
+        $build = '17021302';
         $version = [2, 0, 0];
 
         if ($retBuild) {
