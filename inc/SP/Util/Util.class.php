03ab5e136951ee7a982bd21ae1a3987bc212e2f0
nuxsmin <nuxsmin@syspass.org>
2017-04-04 14:38:39 +0200

    * [FIX] Update preferences when upgrading.

--- inc/SP/Util/Util.class.php
+++ inc/SP/Util/Util.class.php
@@ -407,7 +407,7 @@ class Util
      */
     public static function getVersion($retBuild = false, $normalized = false)
     {
-        $build = 17040302;
+        $build = 17040401;
         $version = [2, 1, 5];
 
         if ($normalized === true) {
