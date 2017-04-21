738db5e892322a470cec84e38135c51c7a7f28d4
nuxsmin <nuxsmin@syspass.org>
2017-04-21 11:03:58 +0200

    * [FIX] Enforced readonly user login for LDAP users

--- inc/SP/Util/Util.class.php
+++ inc/SP/Util/Util.class.php
@@ -407,7 +407,7 @@ class Util
      */
     public static function getVersion($retBuild = false, $normalized = false)
     {
-        $build = 17042005;
+        $build = 17042101;
         $version = [2, 1, 7];
 
         if ($normalized === true) {
