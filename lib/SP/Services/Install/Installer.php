1bcfb6db49c42d2316fa9c332ee72d8c81e99953
nuxsmin <nuxsmin@syspass.org>
2018-07-21 19:59:10 +0200

    * [ADD] Unit testing. Work in progress * [MOD] Code refactoring

--- lib/SP/Services/Install/Installer.php
+++ lib/SP/Services/Install/Installer.php
@@ -54,7 +54,7 @@ class Installer extends Service
      */
     const VERSION = [3, 0, 0];
     const VERSION_TEXT = '3.0-beta';
-    const BUILD = 18071902;
+    const BUILD = 18072101;
 
     /**
      * @var ConfigService
