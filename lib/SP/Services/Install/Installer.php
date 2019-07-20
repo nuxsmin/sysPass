29b56c5e3538c359644bdbefb1a6ffd535d1daba
Rubén D <nuxsmin@syspass.org>
2019-07-20 23:41:15 +0200

    * [MOD] Update version & build number
    Signed-off-by: Rubén D <nuxsmin@syspass.org>

--- lib/SP/Services/Install/Installer.php
+++ lib/SP/Services/Install/Installer.php
@@ -61,8 +61,8 @@ final class Installer extends Service
      * sysPass' version and build number
      */
     const VERSION = [3, 1, 0];
-    const VERSION_TEXT = '3.1-RC4';
-    const BUILD = 19052503;
+    const VERSION_TEXT = '3.1-RC5';
+    const BUILD = 19072001;
 
     /**
      * @var DatabaseSetupInterface
