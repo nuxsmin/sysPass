0ea0a1386055dcf74edb910c9f0f0be5ba80f791
RubénD <nuxsmin@users.noreply.github.com>
2022-06-18 18:01:42 +0200

    fix: Add missing tables to tables list. (#1843)
    Thanks to @Jonher937 for the notice!!
    Closes #1786.
    
    Signed-off-by: Rubén D <nuxsmin@syspass.org>

--- lib/SP/Storage/Database/DatabaseUtil.php
+++ lib/SP/Storage/Database/DatabaseUtil.php
@@ -1,10 +1,10 @@
 <?php
-/**
+/*
  * sysPass
  *
- * @author    nuxsmin
- * @link      https://syspass.org
- * @copyright 2012-2019, Rubén Domínguez nuxsmin@$syspass.org
+ * @author nuxsmin
+ * @link https://syspass.org
+ * @copyright 2012-2022, Rubén Domínguez nuxsmin@$syspass.org
  *
  * This file is part of sysPass.
  *
@@ -19,7 +19,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- *  along with sysPass.  If not, see <http://www.gnu.org/licenses/>.
+ * along with sysPass.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace SP\Storage\Database;
@@ -60,6 +60,9 @@ final class DatabaseUtil
         'UserPassRecover',
         'UserToUserGroup',
         'Plugin',
+        'Track',
+        'ItemPreset',
+        'PluginData',
         'Notification',
         'account_data_v',
         'account_search_v'
