b7284cb358d661260810eea9f39bfa11e59d877b
Rubén D <nuxsmin@syspass.org>
2024-02-10 12:42:54 +0100

    chore(tests): UT for XmlAccountExport
    Signed-off-by: Rubén D <nuxsmin@syspass.org>

---
+++ tests/SPT/Domain/Export/Services/XmlAccountExportTest.php
@@ -0,0 +1,171 @@
+<?php
+/*
+ * sysPass
+ *
+ * @author nuxsmin
+ * @link https://syspass.org
+ * @copyright 2012-2024, Rubén Domínguez nuxsmin@$syspass.org
+ *
+ * This file is part of sysPass.
+ *
+ * sysPass is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * sysPass is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with sysPass.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace SPT\Domain\Export\Services;
+
+use DOMDocument;
+use DOMNodeList;
+use PHPUnit\Framework\MockObject\Exception;
+use PHPUnit\Framework\MockObject\MockObject;
+use RuntimeException;
+use SP\DataModel\ItemData;
+use SP\Domain\Account\Ports\AccountService;
+use SP\Domain\Account\Ports\AccountToTagService;
+use SP\Domain\Common\Services\ServiceException;
+use SP\Domain\Export\Services\XmlAccountExport;
+use SPT\Generators\AccountDataGenerator;
+use SPT\UnitaryTestCase;
+
+/**
+ * Class XmlAccountExportTest
+ *
+ * @group unitary
+ */
+class XmlAccountExportTest extends UnitaryTestCase
+{
+
+    private AccountService|MockObject      $accountService;
+    private AccountToTagService|MockObject $accountToTagService;
+    private XmlAccountExport               $xmlAccountExport;
+
+    /**
+     * @throws Exception
+     * @throws ServiceException
+     */
+    public function testExport()
+    {
+        $account = AccountDataGenerator::factory()->buildAccount();
+        $tag = new ItemData(['id' => self::$faker->randomNumber(3)]);
+
+        $document = new DOMDocument();
+
+        $this->accountService
+            ->expects(self::once())
+            ->method('getAllBasic')
+            ->willReturn([$account]);
+
+        $this->accountToTagService
+            ->expects(self::once())
+            ->method('getTagsByAccountId')
+            ->with($account->getId())
+            ->willReturn([$tag]);
+
+        $out = $this->xmlAccountExport->export($document);
+
+        $this->assertEquals('Accounts', $out->nodeName);
+        $this->assertEquals(9, $out->firstChild->childNodes->count());
+
+        $nodes = [
+            'name' => $account->getName(),
+            'clientId' => $account->getClientId(),
+            'categoryId' => $account->getCategoryId(),
+            'login' => $account->getLogin(),
+            'url' => $account->getUrl(),
+            'notes' => $account->getNotes(),
+            'pass' => $account->getPass(),
+            'key' => $account->getKey(),
+            'tags' => '',
+        ];
+
+        $this->checkNodes($out->firstChild->childNodes, $nodes);
+
+        $tagsNode = $out->firstChild->childNodes->item(8);
+
+        $this->assertEquals('tag', $tagsNode->firstChild->nodeName);
+        $this->assertEquals(
+            $tag->getId(),
+            $tagsNode->firstChild->attributes->getNamedItem('id')->nodeValue
+        );
+    }
+
+    private function checkNodes(DOMNodeList $nodeList, array $nodes): void
+    {
+        $names = array_keys($nodes);
+        $values = array_values($nodes);
+
+        foreach ($names as $key => $nodeName) {
+            $this->assertEquals($nodeName, $nodeList->item($key)->nodeName);
+            $this->assertEquals($values[$key], $nodeList->item($key)->nodeValue);
+        }
+    }
+
+    /**
+     * @throws Exception
+     * @throws ServiceException
+     */
+    public function testExportWithoutAccounts()
+    {
+        $document = new DOMDocument();
+
+        $this->accountService
+            ->expects(self::once())
+            ->method('getAllBasic')
+            ->willReturn([]);
+
+        $this->accountToTagService
+            ->expects(self::never())
+            ->method('getTagsByAccountId');
+
+        $out = $this->xmlAccountExport->export($document);
+
+        $this->assertEquals('Accounts', $out->nodeName);
+        $this->assertEquals(0, $out->childNodes->count());
+    }
+
+    /**
+     * @throws Exception
+     * @throws ServiceException
+     */
+    public function testExportWithException()
+    {
+        $document = new DOMDocument();
+
+        $this->accountService
+            ->expects(self::once())
+            ->method('getAllBasic')
+            ->willThrowException(new RuntimeException('test'));
+
+        $this->accountToTagService
+            ->expects(self::never())
+            ->method('getTagsByAccountId');
+
+        $this->expectException(ServiceException::class);
+        $this->expectExceptionMessage('test');
+        $this->xmlAccountExport->export($document);
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->accountService = $this->createMock(AccountService::class);
+        $this->accountToTagService = $this->createMock(AccountToTagService::class);
+
+        $this->xmlAccountExport = new XmlAccountExport(
+            $this->application,
+            $this->accountService,
+            $this->accountToTagService
+        );
+    }
+}
